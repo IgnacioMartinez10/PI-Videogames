5a132e0118c6bf35b744ef4c09649267988a3ad5
Ignacio Martinez <ignaciomartinez923@gmail.com>
2023-12-13 20:02:29 -0300

    upgrade

--- client/src/components/Card/card.component.jsx
+++ client/src/components/Card/card.component.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import "./card.styles.css";
 
 function Card(props) {
-  const { id, name, image, genres, reviews } = props;
+  const { id, name, image, genres } = props;
   return (
     <Link to={`/home/${id}`}>
       <div
